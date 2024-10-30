16919e77c8a3f9007655887f5d77665bb567d01d
Ese Amadasun <amadasunese@gmail.com>
2024-10-30 14:34:19 +0100

    week one task completed

---
+++ README.md
@@ -0,0 +1,1 @@
+## Python Programming specialization tasks
---
+++ week-one-intro-to-python/user_input.py
@@ -0,0 +1,7 @@
+# program that asks the user for their name, age, and location and then prints out a personalized message.
+
+name = input('Enter your name ')
+age = input('Enter your age ')
+location = input('Enter your location ')
+
+print(f'Hello {name}, you are {age} years old and you live in {location}')
